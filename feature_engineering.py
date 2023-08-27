733c82b02d82a49f4a6c2cf6869fb8870bd75743
Tolga <46111780+tlgakpln@users.noreply.github.com>
2023-08-27 22:33:52 +0300

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -15,31 +15,22 @@ def edit_dtypes(data):
 
 
 def engineer_new_features(data):
-    # Create a new feature for total square footage
     data['TotalSF'] = data['TotalBsmtSF'] + data['1stFlrSF'] + data['2ndFlrSF']
 
-    # Total number of bathrooms
     data['TotalBathrooms'] = data['FullBath'] + 0.5 * data['HalfBath'] + data['BsmtFullBath'] + 0.5 * data['BsmtHalfBath']
 
-    # Age of the house at the time of sale
     data['HouseAge'] = data['YrSold'] - data['YearBuilt']
 
-    # Age of the garage at the time of sale
     data['GarageAge'] = data['YrSold'] - data['GarageYrBlt']
 
-    # Total porch area
     data['TotalPorchSF'] = data['OpenPorchSF'] + data['EnclosedPorch'] + data['3SsnPorch'] + data['ScreenPorch']
 
-    # Ratio of living area to lot area
     data['LivLotRatio'] = data['GrLivArea'] / data['LotArea']
 
-    # Interaction between quality and condition
     data['QualCondInteraction'] = data['OverallQual'] * data['OverallCond']
 
-    # Total number of rooms
     data['TotalRooms'] = data['TotRmsAbvGrd'] + data['FullBath'] + data['HalfBath']
 
-    # Total square footage of the house including basement, garage, and porch
     data['TotalHouseSF'] = data['TotalSF'] + data['TotalPorchSF']
 
     return data
